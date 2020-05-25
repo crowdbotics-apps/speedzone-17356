a5123c08ecbbec98f64f15a4bde6df2b9acc98e1
Crowdbotics <team@crowdbotics.com>
2020-05-25 03:24:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/speedzone_17356/MainActivity.java
@@ -0,0 +1,15 @@
+package com.speedzone_17356;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "speedzone_17356";
+    }
+}
